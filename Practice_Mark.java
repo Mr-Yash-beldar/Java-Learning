a5a697a1c80dbe52d8fb3c5b662e5be9fd072a88
Yash-Beldar <yashodip2003beldar@gmail.com>
2022-09-15 13:09:34 +0530

    Percentage of marks code in Java

---
+++ Practice_Mark.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Practice_Mark {
+    public static void main(String[] args) {
+        System.out.println("\nPercentage of marks by Java\n");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the total marks of each Subject");
+        int tm;
+        tm=input.nextInt();
+        float s1, s2, s3, s4, s5, total, per;
+        System.out.println("Enter marks of five subjects ");
+        s1 = input.nextFloat();
+        s2 = input.nextFloat();
+        s3 = input.nextFloat();
+        s4 = input.nextFloat();
+        s5 = input.nextFloat();
+        total = s1 + s2 + s3 + s4 + s5;
+        per = total / 5;
+        System.out.println("Your Percentage is :  ");
+        System.out.println(per);
+
+    }
+}
