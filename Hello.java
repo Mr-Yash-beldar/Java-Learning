9e56f22bf85629e35e0767c516ffc09bb83162c7
Yash-Beldar <yashodip2003beldar@gmail.com>
2022-09-15 13:58:40 +0530

    Greeting program in java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+//Author--Yashodip Beldar
+//Date--14-09-2022
+//Purpose--Greeting program in java.
+public class Hello {
+        public static void main(String[] args) {
+        System.out.println("\nHi My name is Yashodip Beldar\n");
+        System.out.println("What is your name:");
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();
+        System.out.println("Hello! "+name+ ", Have a good day.");
+
+
+    }
+    
+}
