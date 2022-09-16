655c628e6373345d78177e8bc403cfa5bdd2f923
Yash-Beldar <yashodip2003beldar@gmail.com>
2022-09-16 14:32:08 +0530

    to check number is integer or not

---
+++ Check_int.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//Author--Yashodip Beldar
+//Date--15-09-2022
+//Purpose--to check entered number is integer or not.
+
+public class Check_int {
+    public static void main(String[] args) {
+        System.out.println("\nHi My name is Yashodip Beldar\n");
+        System.out.println("Enter your number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+
+    }
+
+}
