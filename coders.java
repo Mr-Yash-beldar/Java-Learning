ac11c2f10ff04551603d4600523f05745f3deeb7
Yash-Beldar <yashodip2003beldar@gmail.com>
2022-09-24 20:24:40 +0530

    Program from coders group for printing

---
+++ coders.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+//Date--17.09.2022
+//Purpose-- Java Program to Take User input
+
+public class coders {
+     public static void main(String[] args) {
+     Scanner input= new Scanner(System.in);
+     System.out.println("\nEnter interger number :");
+     int a;
+     a=input.nextInt();
+     System.out.println("\nEnter Floating point number: ");
+     double b;
+     b=input.nextDouble();
+     System.out.println("\nEnter any String");
+     String name=input.nextLine();
+     System.out.println("\nHello! "+name+ "\n");
+     System.out.println("Your Double is: "+b+"\n");
+     System.out.println("Your Interger is: "+a+"\n");
+    //   System.out.println(name);
+    //  System.out.println(b);
+    //  System.out.println(a);
+
+    }
+}
