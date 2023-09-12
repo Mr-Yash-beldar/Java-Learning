96e5ebbd1daed238fb82b9197100a83565cd7525
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-09-12 15:52:40 +0530

    marks and percentage

--- Practice_Mark.java
+++ Practice_Mark.java
@@ -5,8 +5,8 @@ class Practice_Mark {
         System.out.println("\nPercentage of marks by Java\n");
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the total marks of each Subject");
-        int tm;
-        tm=input.nextInt();
+        // int tm;
+        // tm=input.nextInt();
         float s1, s2, s3, s4, s5, total, per;
         System.out.println("Enter marks of five subjects ");
         s1 = input.nextFloat();
@@ -18,6 +18,6 @@ class Practice_Mark {
         per = total / 5;
         System.out.println("Your Percentage is :  ");
         System.out.println(per);
-
+        input.close();
     }
 }
